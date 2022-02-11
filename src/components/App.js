55273ba383800bcf9c1917e4638cd86153855811
GrazianoMono <graziano@mono.studio>
2022-02-11 18:55:25 +0100

    feat: added useEffect to check the cookie

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import '../scss/App.scss';
-import React, { useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import Login from '../components/Login';
 import Dashboard from './Dashboard';
+import Cookies from 'js-cookie';
 
 function App() {
 	const loginReducer = (state, action) => {
 		switch (action.type) {
+			case 'cookieSuccess':
+				console.log('Cookie valido');
+				return { logged: true };
 			case 'success':
+				Cookies.set('logged', 'success', { expires: 2 });
 				return { logged: true };
 			case 'incorrect':
 				return { logged: false, showError: true };
@@ -33,6 +38,12 @@ function App() {
 	const handleEmailChange = (e) => {
 		setEmail(e.target.value);
 	};
+
+	useEffect(() => {
+		const cookie = Cookies.get('logged');
+		console.log(cookie === 'success');
+		if (cookie === 'success') dispatch({ type: 'cookieSuccess' });
+	}, []);
 
 	return (
 		<div className="App">
