af08cf51cd3d04e58622216f3418adc8db080d69
GrazianoMono <graziano@mono.studio>
2022-02-11 17:20:23 +0100

    feat: included in App functions to handle login

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
 import '../scss/App.scss';
+import React, { useReducer, useState } from 'react';
+import Login from '../components/Login';
+import Dashboard from './Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+	const loginReducer = (state, action) => {
+		switch (action.type) {
+			case 'success':
+				return { logged: true };
+			case 'incorrect':
+				console.log('Suca e io non ti loggo perché hai sbagliato');
+				return { logged: false };
+			default:
+				return state;
+		}
+	};
+
+	const [logged, dispatch] = useReducer(loginReducer, { logged: false });
+
+	const handleSubmit = (e) => {
+		if (password === '123456' && email === 'guest@mono.studio')
+			dispatch({ type: 'success' });
+		else dispatch({ type: 'incorrect' });
+		e.preventDefault();
+	};
+
+	const [password, setPassword] = useState('');
+	const handlePasswordChange = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const [email, setEmail] = useState('');
+	const handleEmailChange = (e) => {
+		setEmail(e.target.value);
+	};
+
+	return (
+		<div className="App">
+			{!logged.logged && (
+				<Login
+					handleSubmit={handleSubmit}
+					handleEmailChange={handleEmailChange}
+					handlePasswordChange={handlePasswordChange}
+					password={password}
+					email={email}
+				/>
+			)}
+			{logged.logged && <Dashboard />}
+		</div>
+	);
 }
 
 export default App;
